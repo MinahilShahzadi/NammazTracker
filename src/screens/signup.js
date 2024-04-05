0b94cc28b4e7b3576cc6a6a3923cc4d2331f30d3
MinahilShahzadi <meenufatima123@gmail.com>
2024-04-06 01:06:40 +0500

    DOB valu 1

--- src/screens/signup.js
+++ src/screens/signup.js
@@ -1,12 +1,31 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Image, Pressable } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const Signup = () => {
 
     const inputStyle = "border-b-2 border-b-blue-400 p-2 rounded-md my-2";
 
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+    const [dobValue, setDobValue ] = useState("");
+
+    const showDatePicker = () => {
+    setDatePickerVisibility(true);
+    };
+
+    const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+    };
+
+    const handleConfirm = (date) => {
+    console.warn("A date has been picked: ", date);
+    setDobValue(date.toString());
+    hideDatePicker();
+    };
+ 
     return (
         <View className={"flex flex-1"}>
-            <View className={"flex-1 items-center justify-end bg-pink-300"}>
+            <View className={"flex-auto items-center justify-end bg-pink-300 "}>
                 <TouchableOpacity>
                     <Image
                     source={require("../../assets/icon.png")}
@@ -20,16 +39,29 @@ const Signup = () => {
                 <TextInput  placeholder="Email" className={inputStyle}/>
                 <TextInput  placeholder="Password" className={inputStyle}/>
                 <TextInput  placeholder="Confirm Password" className={inputStyle}/>
-                <TextInput  placeholder="DOB" className={inputStyle}/>
-                <TextInput  placeholder="Gender" className={inputStyle}/>
+                
+                <Pressable className={inputStyle} onPress={showDatePicker}>
+                    <Text>{dobValue === "" ? "DOB": dobValue}</Text>
+                </Pressable>
+                <Pressable className={inputStyle}>
+                    <Text>gender</Text>
+                </Pressable>
             </View>
-            
+ 
             <View className={"flex-none bg-purple-400 p-5"}>
-                <TouchableOpacity className={"items-center p-5 rounded-xl bg-red-300"}>
+                <TouchableOpacity  className={"items-center p-5 rounded-xl bg-red-300"}>
                     <Text className={" text-lg text-red-900 font-extrabold"}>Signup</Text>
                 </TouchableOpacity>
-            </View>    
+            </View> 
+
+            <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                onConfirm={handleConfirm}
+                onCancel={hideDatePicker}
+        	/>   
         </View>
+
     );
     
 };
